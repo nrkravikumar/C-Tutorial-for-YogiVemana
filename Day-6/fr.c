79cf757a565a79a4127ce8f6b504a19872ff0e6e
NRK <rravikumar34@gmail.com>
2020-12-26 08:15:28 +0530

    Day-6 C Programming Updated

---
+++ Day-6/fr.c
@@ -0,0 +1,15 @@
+//~ numbers printing upto n
+
+#include<stdio.h>
+
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",i+1);
+	}
+	//~ printf("%d",i);
+	return 0;
+}
